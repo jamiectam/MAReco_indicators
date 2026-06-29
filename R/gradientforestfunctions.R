5cbdfe8696d915a70e72c09cb72417a23424cd64
jamiectam <jamiectam.phd@gmail.com>
2026-06-29 09:41:13 -0300

    +thresholds gam tested with full suite of indicators pressures. Still issues with GAM derivatives. +gf work

--- R/gradientforestfunctions.R
+++ R/gradientforestfunctions.R
@@ -548,20 +548,24 @@ spCompPlot <- function (obj, imp.vars = NULL, imp.vars.names = imp.vars, leg.pos
         outer = TRUE)
 }
 
-impPlot <- function (obj, ..., las = 1, cex.axis = 0.7, cex.names = cex.axis, 
-                     horiz = TRUE) 
+impPlot <- function(obj,
+                    las = 1,
+                    cex.axis = 0.7,
+                    cex.names = cex.axis,
+                    horiz = TRUE,
+                    ...) 
 {
-  #     imp.a <- importance(obj, "Accuracy")
   imp.w <- importance(obj, "Weighted")
-  #     o.a <- order(imp.a)
   o.w <- order(imp.w)
-  #     barplot(imp.a[o.a], names = names(imp.a[o.a]), horiz = horiz, 
-  #             main = "Accuracy importance", las = las, cex.axis = cex.axis, 
-  #             cex.names = cex.names, ...)
-  barplot(imp.w[o.w], names = names(imp.w[o.w]), horiz = horiz, 
-          las = 1, 
-          #             main = expression(paste(R^2, " weighted importance")),
-          main = "",
-          las = las, cex.axis = cex.axis, cex.names = cex.names, 
-          ...)
+  
+  barplot(
+    imp.w[o.w],
+    names = names(imp.w[o.w]),
+    horiz = horiz,
+    las = las,
+    main = "",
+    cex.axis = cex.axis,
+    cex.names = cex.names,
+    ...
+  )
 }
